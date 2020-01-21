21e06459c1864c8427fdf7f0fa7b9c2b070c4da4
Amanda Higgins <amanda.n.higgins@gmail.com>
2020-01-20 19:07:55 -0500

    almost final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,32 @@
 // The player will be shown a random number at the start of the game.
-
 //  When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
-
 // Crystals are between 1-12
 // Random number is between 19-120
-
 //   Your game will hide this amount until the player clicks a crystal.
 //   When they do click one, update the player's score counter.
-
 // The player wins if their total score matches the random number from the beginning of the game.
-
 // The player loses if their score goes above the random number.
-
 // The game restarts whenever the player wins or loses.
-
 // When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
-
-
 
 // variables
 var randomNumber = $("randomNumber").val();
-var crystalOne = $("randomNumber").val();
+var crystalOne = 0;
 var crystalTwo = $("randomNumber").val();
 var crystalThree = $("randomNumber").val();
 var crystalFour = $("randomNumber").val();
-var userScore = [];
-var totalScore = [];
+var userScore = 0;
+var totalScore = 0;
 var wins = 0;
 var losses = 0;
-
-
 
 // generate the random goal number between 19-120
 
 function randomNumberGoal() {
-    randomNumber = Math.floor(Math.random() * 50 + 1)
-    console.log(randomNumberGoal);
+    totalScore = Math.floor(Math.random() * (120-19+1)) + 19;
+    console.log(totalScore);
     $("#yourGoal").empty();
-    $("#yourGoal").append("<h2>Your goal: " + randomNumber + "</h2>");
+    $("#yourGoal").append("<h2>Your goal: " + totalScore + "</h2>");
 }
 
 // run randomNumber Goal function
@@ -46,13 +35,10 @@ randomNumberGoal ();
 
 // crystal values
 function crystalValues() {
-    crystalOne = Math.floor(Math.random() * 5 + 1);
-
-    crystalTwo = Math.floor(Math.random() * 10 + 1);
-
-    crystalThree = Math.floor(Math.random() * 15 + 1);
-
-    crystalFour = Math.floor(Math.random() 20 + 1);
+    crystalOne = Math.floor(Math.random() * (12-1+1)) + 1;
+    crystalTwo = Math.floor(Math.random() * (12-1+1)) + 1;
+    crystalThree = Math.floor(Math.random() * (12-1+1)) + 1;
+    crystalFour = Math.floor(Math.random() * (12-1+1)) + 1;
 }
 
 // run crystal values function
@@ -60,42 +46,86 @@ crystalValues();
 
 // reset - empty out functions
 function reset() {
-    $("#yourgoal").empty();
+    totalScore = 0;
+    userScore = 0;
+    crystalOne = 0;
+    crystalTwo = 0;
+    crystalThree = 0;
+    crystalFour = 0;
+    crystalValues();
     randomNumberGoal();
-    crystalValues ();
-    totalScore ();
-}
-
-// adding crystals
-function addScore() {
-
-    $("#yourScore").append("<h2>Your Score: " + randomNumber + "</h2>");
-
 }
 
 // linking crystal ids to functions and adding together
 
 // crystal 1
 $("#crystal1").click(function() {
-    
-}
+    // totalScore.push(crystalOne);
+    // addScore();
+    // ifWinner();
+    // ifLose();
+
+    userScore = crystalOne + userScore
+    $("#userScore").html(userScore)
+    checkScore()
+   console.log(userScore)
+});
 
 //crystal 2
 $("#crystal2").click(function() {
+    // totalScore.push(crystalTwo);
+    // addScore();
+    // ifWinner();
+    // ifLose();
+    userScore = crystalTwo + userScore
+    $("#userScore").html(userScore)
+    checkScore()
+   console.log(userScore)
     
-}
+});
 
 //crystal 3
 $("#crystal3").click(function() {
-    
-}
+    // totalScore.push(crystalThree);
+    // addScore();
+    // ifWinner();
+    // ifLose();
+    userScore = crystalThree + userScore
+    $("#userScore").html(userScore)
+    checkScore()
+   console.log(userScore)
+});
 
 //crystal 4
 $("#crystal4").click(function() {
-    
-}
+    // totalScore.push(crystalFour);
+    // addScore();
+    // ifWinner();
+    // ifLose();
+    userScore = crystalFour + userScore
+    $("#userScore").html(userScore)
+    checkScore()
+   console.log(userScore)
+});
 
 //run add score
 addScore();
 
 //win or loss
+
+function checkScore(){
+    if (userScore === totalScore){
+        console.log("you win")
+        wins++;
+        $("#winScore").html(wins)
+        reset();
+    }
+
+    if (userScore > totalScore){
+        console.log("you lost")
+        losses++;
+        $("#lossScore").html(losses)
+        reset();
+    }
+}
+
